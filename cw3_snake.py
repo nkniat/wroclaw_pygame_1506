63561a3cb9faa1e4af13dcf35998aad3aaca1d49
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-06-15 14:48:19 +0200

    waz - prawie dziala :)

--- cw3_snake.py
+++ cw3_snake.py
@@ -1,8 +1,9 @@
 import pygame
-pygame.init()
+import random
 
-WIDTH, HEIGHT = 480, 480
+WIDTH, HEIGHT = 500, 500
 GREEN = (0, 255, 0)
+RED = (255, 0, 0)
 
 UP = (0, -1)
 DOWN = (0, 1)
@@ -10,6 +11,8 @@ LEFT = (-1, 0)
 RIGHT = (1, 0)
 
 SIZE = 20
+WIDTH_BOX = WIDTH // SIZE
+HEIGHT_BOX = HEIGHT // SIZE
 
 class Snake():
     def __init__(self):
@@ -65,21 +68,66 @@ class Snake():
 
 class Food():
     def __init__(self):
-        pass
+        self.position = (0,0)
+        self.color = RED
+        self.random_position()
 
     def random_position(self):
-        pass
+        self.position = (random.randint(0, WIDTH_BOX - 1) * SIZE, random.randint(0, HEIGHT_BOX - 1) * SIZE)
 
-    def draw(self):
-        pass
+    def draw(self, win):
+        r = pygame.Rect((self.position[0], self.position[1]), (SIZE, SIZE))
+        pygame.draw.rect(win, self.color, r)
 
 
-def draw_board():
-    pass
+def draw_board(win):
+    win.fill("Black")
+    for i in range(0, WIDTH_BOX):
+        pygame.draw.line(win, (128, 128, 128), (0, i * SIZE), (WIDTH, i * SIZE))
+
+    for j in range(0, HEIGHT_BOX):
+        pygame.draw.line(win, (128, 128, 128), (j * SIZE, 0), (j * SIZE, HEIGHT))
 
 
 def main():
-    pass
+    pygame.init()
+
+    win = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Snake")
+
+    draw_board(win)
+
+    snake = Snake()
+    food = Food()
+
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    snake.direction(UP)
+                if event.key == pygame.K_DOWN:
+                    snake.direction(DOWN)
+                if event.key == pygame.K_LEFT:
+                    snake.direction(LEFT)
+                if event.key == pygame.K_RIGHT:
+                    snake.direction(RIGHT)
+
+        snake.move()
+
+        if snake.head_position() == food.position:
+            snake.eat()
+            food.random_position()
+
+        draw_board(win)
+        snake.draw(win)
+        food.draw(win)
+        #win.blit(win, (0,0))
+
+        pygame.display.update()
+    pygame.quit()
 
 
 if __name__ == "__main__":
