08e9bcc4a9101eb007b3afb53b37b91c5f2052ab
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-06-15 10:32:08 +0200

    podstawy + obsluga klawiszy

--- cw1_podstawy_pygame.py
+++ cw1_podstawy_pygame.py
@@ -7,7 +7,7 @@ pygame.display.set_caption("Moja pierwsza gra")  #nazwa okienka
 # dodajemy framerate
 clock = pygame.time.Clock()
 
-X, Y = 10, 10
+X, Y = 400, 200
 
 while True:
     # 1. sprawdzanie inputu od użytkownika
@@ -18,17 +18,33 @@ while True:
 
     # 2a. rysowanie elementów
     #pygame.draw.rect(screen, "White", (X, Y, 10, 10))
-    pygame.draw.line(screen, (255, 0, 0), (X, Y), (X, Y + 200), 5) # pion
-    pygame.draw.line(screen, (0, 255, 0), (X, Y), (X + 200, Y), 5) # poziom
+    #pygame.draw.line(screen, (255, 0, 0), (X, Y), (X, Y + 200), 5) # pion
+    #pygame.draw.line(screen, (0, 255, 0), (X, Y), (X + 200, Y), 5) # poziom
     # fraktale TODO
 
-    # 2b. renderowanie czcionek
-    # definicja czcionki
-    font = pygame.font.SysFont('Arial', 24)
-    # renderowanie napisu
-    label = font.render('Hello World', 1, 'White')
-    # wyswietlenie napisu na ekran
-    screen.blit(label, (X + 50, Y + 50))
+    # # 2b. renderowanie czcionek
+    # # definicja czcionki
+    # font = pygame.font.SysFont('Arial', 24)
+    # # renderowanie napisu
+    # label = font.render('Hello World', 1, 'White')
+    # # wyswietlenie napisu na ekran
+    # screen.blit(label, (X + 50, Y + 50))
+
+    STEP = 5
+    keys = pygame.key.get_pressed()
+    #print(keys)
+
+    if keys[pygame.K_LEFT]:  #lewa strzałka na klawiaturz
+        X -= STEP
+    if keys[pygame.K_RIGHT]:  #prawa strzałka na klawiaturz
+        X += STEP
+    if keys[pygame.K_UP]:  #górna strzałka na klawiaturz
+        Y -= STEP
+    if keys[pygame.K_DOWN]:  #dolna strzałka na klawiaturz
+        Y += STEP
+
+    screen.fill('Black')
+    pygame.draw.rect(screen, "White", (X, Y, 10, 10))
 
     # 3. ciągły update ekranu
     pygame.display.update()
