3f559f036dde64d19501f7e9d7c14505cd73da79
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-06-15 13:23:58 +0200

    szkielet programu Snake

---
+++ cw3_snake.py
@@ -0,0 +1,46 @@
+import pygame
+pygame.init()
+
+class Snake():
+    def __init__(self):
+        pass
+
+    def head_position(self):
+        pass
+
+    def direction(self):
+        pass
+
+    def move(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def eat(self):
+        pass
+
+
+class Food():
+    def __init__(self):
+        pass
+
+    def random_position(self):
+        pass
+
+    def draw(self):
+        pass
+
+def draw_board():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
