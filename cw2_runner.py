b14ddda57ef43d54a0b66ab85eb3d6bb0040b18a
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-06-15 12:09:56 +0200

    wznowienie gry

--- cw2_runner.py
+++ cw2_runner.py
@@ -6,6 +6,8 @@ screen = pygame.display.set_mode((600, 400))  #rozmiar okna
 pygame.display.set_caption("Moja pierwsza gra")  #nazwa okienka
 # dodajemy framerate
 clock = pygame.time.Clock()
+game_active = True
+font = pygame.font.SysFont("Arial", 24)
 
 background_surface = pygame.image.load('images_PG/background.png').convert()
 
@@ -17,32 +19,50 @@ player_surface = pygame.image.load('images_PG/girl_stay.png').convert_alpha()
 player_rect = player_surface.get_rect(midbottom=(50, 350))
 player_gravity = 0
 
+game_msg = font.render("Naciśnij spację, żeby zagrać dalej", 1, "White")
+game_msg_rect = game_msg.get_rect(center=(300, 200))
+
 while True:
     # 1. sprawdzanie inputu od użytkownika
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE and player_rect.bottom == 350:
-                #print('nacisnieto spacje')
-                player_gravity = -20
 
-    screen.blit(background_surface, (0, 0))
+        if game_active:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and player_rect.bottom == 350:
+                    #print('nacisnieto spacje')
+                    player_gravity = -20
 
-    mushroom_rect.x -= 5
-    if mushroom_rect.x < -100:
-        mushroom_rect.x = 700
-    screen.blit(mushroom_surface, mushroom_rect)
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+                mushroom_rect.left = 700
 
-    player_gravity += 1
-    player_rect.y += player_gravity
-    if player_rect.bottom >= 350:
-        player_rect.bottom = 350
-    screen.blit(player_surface, player_rect)
+    if game_active:
+        screen.blit(background_surface, (0, 0))
 
-    # if player_rect.colliderect(mushroom_rect):
-    #     print('Zderzenie')
+        mushroom_rect.x -= 5
+        if mushroom_rect.x < -100:
+            mushroom_rect.x = 700
+        screen.blit(mushroom_surface, mushroom_rect)
+
+        player_gravity += 1
+        player_rect.y += player_gravity
+        if player_rect.bottom >= 350:
+            player_rect.bottom = 350
+        screen.blit(player_surface, player_rect)
+
+        if player_rect.colliderect(mushroom_rect):
+            #print('Zderzenie')
+            # pygame.quit()
+            # exit()
+            game_active = False
+    else:
+        screen.fill("Black")
+        screen.blit(game_msg, game_msg_rect)
+
 
     # 3. ciągły update ekranu
     pygame.display.update()
